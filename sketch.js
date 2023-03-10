cc8c02f6efef635d3faa66453c8f13baa5e33b3d
SeaSunPT <postico.andre@gmail.com>
2023-03-10 17:11:03 +0000

    sketchpad working with reset and change of grid size

--- sketch.js
+++ sketch.js
@@ -1,41 +1,63 @@
-let columns = 16;
-let rows = 16;
+let size = 16;
 
-const grid = document.createElement('div');
-grid.className = 'grid';
-for (let c = 0; c < columns; ++c) {
-    let column = document.createElement('div'); // create column
-    column.className = 'column';
-    for (let r = 0; r < rows; ++r) {
-        let row = document.createElement('div'); // create row
-        row.className = 'row';
-        row.addEventListener('click', function (event) {
-            row.style.backgroundColor = "black";
-        });
-        row.addEventListener('mouseover', function (event) {
-            row.style.backgroundColor = "black";
-        });
-        column.appendChild(row); // append row in column
+const removeElementsByClass = (className) => {
+    const elements = document.getElementsByClassName(className);
+    while (elements.length > 0) {
+        elements[0].parentNode.removeChild(elements[0]);
     }
-    grid.appendChild(column); // append column inside grid
 }
-document.body.appendChild(grid);
+
+const generateInitialGrid = (size) => {
+    let grid;
+    if (document.getElementById('container') === null) {
+        grid = document.createElement('div');
+        grid.className = 'grid';
+        grid.id = 'container'
+    }
+    else grid = document.getElementById('container');
+
+    if (document.getElementById('row') === null) {
+        removeElementsByClass('row');
+    }
+    if (document.getElementById('column') === null) {
+        removeElementsByClass('column');
+    }
+
+    for (let c = 0; c < size; ++c) {
+
+        let column = document.createElement('div'); // create column
+        column.className = 'column';
+        for (let r = 0; r < size; ++r) {
+            let row = document.createElement('div'); // create row
+            row.className = 'row';
+            let squareSize = 640 / size;
+            row.id = c + '-' + r
+            row.style = `width: ${squareSize}px; height: ${squareSize}px; background: whitesmoke; `
+            row.addEventListener('mouseover', function (event) {
+                row.style.backgroundColor = "black";
+            });
+            column.appendChild(row); // append row in column
+        }
+        grid.appendChild(column); // append column inside grid
+    }
+    document.body.appendChild(grid);
+}
+
+const generateGrid = (size) => {
+    generateInitialGrid(size)
+}
 
 
-// for (let column = 1; column < 17; column++) {
+generateInitialGrid(size);
 
-//     let columnDiv = document.createElement("div");
-//     columnDiv.setAttribute("id", column);
-//     columnDiv.setAttribute("class", "column" + column);
-//     columnDiv.setAttribute("style", "height: 40px; width: 40px; float: left; background-color: #000000");
-//     appContainer.appendChild(columnDiv);
-
-//     for (let row = 1; row < 17; row++) {
-
-//         let rowDiv = document.createElement("div");
-//         rowDiv.setAttribute("id", row + column);
-//         rowDiv.setAttribute("class", "row" + column);
-//         columnDiv.setAttribute("style", "height: 40px; width: 40px; float: left; background-color: #ff0000");
-//         appContainer.appendChild(rowDiv);
-//     }
-// }
+const changeSizeButton = document.getElementById("change-size");
+changeSizeButton.addEventListener('click', function (event) {
+    let gridSize = prompt("Choose size of grid (to 100px): ", 16);
+    while (gridSize > 100 || gridSize <= 16) {
+        console.log("while");
+        gridSize = prompt("Choose size of grid (between 16 and 100): ", 16);
+    }
+    if (gridSize <= 100 && gridSize > 16) {
+        generateGrid(gridSize);
+    }
+});
